2ec68eaab29b4ae0543c0d17073994e83f76af0f
williamtanuwijaya <will.tw0807@gmail.com>
2023-03-14 13:51:21 +0700

    update the challenge 1 is complete

--- UcokExplorer_Pigtails/src/App.java
+++ UcokExplorer_Pigtails/src/App.java
@@ -1,7 +1,21 @@
 import java.io.*;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        System.out.print("Masukkan angka : ");
+        int jumlah = Integer.parseInt(br.readLine());
+        System.out.println(test(jumlah));
+    }
+
+    static int test(int jumlah) {
+        if (jumlah == 0) {
+            return 0;
+        } else if (jumlah % 2 == 0) {
+            return 3 + test(jumlah - 1);
+        } else {
+            return 2 + test(jumlah - 1);
+        }
     }
 }
