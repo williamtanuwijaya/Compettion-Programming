ebcc019cbccc47ba975ef6d3b00895b5dda121e8
williamtanuwijaya <will.tw0807@gmail.com>
2023-03-14 13:38:00 +0700

    we create the challenge 1 is pigtails

---
+++ UcokExplorer_Pigtails/src/App.java
@@ -0,0 +1,7 @@
+import java.io.*;
+public class App {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+    }
+}
